8d6650eab546248241dfb1db88e7adda855bbb7c
Andreas Pritschet <apri@kupferwerk.net>
2014-09-03 16:23:18 +0200

    Added Button to show and hide TargetImage

---
+++ Ismar_BaseProject/Assets/HandleTarget.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class HandleTarget : MonoBehaviour {
+
+    public GameObject target;
+    public Text text;
+
+    public void ShowTarget()
+    {
+        target.SetActive(!target.activeSelf);
+        if (target.activeSelf)
+            text.text = "Hide Target";
+        else
+            text.text = "Show Target";
+    }
+}
