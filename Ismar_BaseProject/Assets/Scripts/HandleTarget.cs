55c8e8e7065b7bbe8ed5cf4eb4987451c91780ec
Andreas Pritschet <apri@kupferwerk.net>
2014-09-04 17:32:30 +0200

    Right parts of image will be shown on puzzle

--- Ismar_BaseProject/Assets/Scripts/HandleTarget.cs
+++ Ismar_BaseProject/Assets/Scripts/HandleTarget.cs
@@ -2,23 +2,31 @@
 using UnityEngine.UI;
 using System.Collections;
 
-public class HandleTarget : MonoBehaviour {
+public class HandleTarget : MonoBehaviour
+{
 
-    public GameObject target;
+    public GameObject target1;
+    public GameObject target2;
+    public GameObject target3;
     public Text text;
 
     public void ShowTarget()
     {
         GameObject[] cubeObjects = GameObject.FindGameObjectsWithTag("Cube");
 
-        foreach(GameObject cube in cubeObjects)
+        foreach (GameObject cube in cubeObjects)
         {
             cube.GetComponent<RotateCube>().ChangeLocation();
         }
 
-        target.SetActive(!target.activeSelf);
+        if (PlayerPrefsX.GetBool("GPS_key2", false))
+            target1.SetActive(!target1.activeSelf);
+        if (PlayerPrefsX.GetBool("GPS_key1", false))
+            target2.SetActive(!target2.activeSelf);
+        if (PlayerPrefsX.GetBool("GPS_key3", false))
+            target3.SetActive(!target3.activeSelf);
 
-        if (target.activeSelf)
+        if (target1.activeSelf || target2.activeSelf || target3.activeSelf)
             text.text = "Hide Target";
         else
             text.text = "Show Target";
