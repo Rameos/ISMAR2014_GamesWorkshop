5e58277e8ff57deffd27d34d3d7d70cc61085c9e
Ozelotl <s.delonge@tum.de>
2014-09-04 17:02:30 +0200

    AddedFirstScreens

---
+++ Ismar_BaseProject/Assets/Scripts/PlaneScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaneScript : MonoBehaviour {
+
+	public static int lives;
+
+	// Use this for initialization
+	void Start () {
+		lives = 3;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+
+		Destroy(other.gameObject);lives--;
+				
+	}
+
+	void OnGUI()
+	{
+		GUI.TextArea (new Rect (0f, 0f, 50f, 25f), "" + lives);
+	}
+}
