d659b7840a15c4dd82e8247b8880549f609c6bcc
Aytek Aman <aytekaman@gmail.com>
2014-09-04 15:41:56 +0200

    Added non-working collision system

--- Ismar_BaseProject/Assets/Aytek Scripts/ForceGame.cs
+++ Ismar_BaseProject/Assets/Aytek Scripts/ForceGame.cs
@@ -7,19 +7,30 @@ public class ForceGame : MonoBehaviour
     List<GameObject> forceFields = new List<GameObject>();
     public GameObject forceFieldPrefab;
     GameObject arCamera;
+
+    public Texture crosshairTexture;
+    public float crossHairRadius = 20;
+
     void Start()
     {
         arCamera = GameObject.Find("ARCamera");
 	}
-	
-	void Update()
+
+    void OnGUI()
+    {
+        Rect rect = new Rect(Screen.width / 2 - crossHairRadius, Screen.height / 2 - crossHairRadius, crossHairRadius * 2, crossHairRadius * 2);
+
+        //GUI.DrawTexture(rect, crosshairTexture);
+    }
+
+    void CreateForceFieldUsingAngle()
     {
         int i = 0;
         while (i < Input.touchCount)
         {
             if (Input.GetTouch(i).phase == TouchPhase.Ended)
             {
-                Vector2 touchPosition =  Input.GetTouch(i).position;
+                Vector2 touchPosition = Input.GetTouch(i).position;
                 Ray ray = arCamera.camera.ScreenPointToRay(touchPosition);
                 RaycastHit hit;
 
@@ -50,5 +61,74 @@ public class ForceGame : MonoBehaviour
 
             ++i;
         }	
+    }
+
+    Vector3 lastWorldPosition = Vector3.zero;
+
+    void CreateForceFieldUsingSweepMotion()
+    {
+        int i = 0;
+        while (i < Input.touchCount)
+        {
+            if (Input.GetTouch(i).phase == TouchPhase.Began)
+            {
+                Vector2 touchPosition = Input.GetTouch(i).position;
+                Ray ray = arCamera.camera.ScreenPointToRay(touchPosition);
+                RaycastHit hit;
+
+                if (Physics.Raycast(ray, out hit))
+                {
+                    lastWorldPosition = hit.point;
+                }
+            }
+
+            ++i;
+        }
+
+        i = 0;
+        while (i < Input.touchCount)
+        {
+            if (Input.GetTouch(i).phase == TouchPhase.Ended)
+            {
+                Vector2 touchPosition = Input.GetTouch(i).position;
+                Ray ray = arCamera.camera.ScreenPointToRay(touchPosition);
+                RaycastHit hit;
+
+                if (Physics.Raycast(ray, out hit))
+                {
+                    Vector3 rotation = Vector3.zero;
+                    Vector3 direction = hit.point - lastWorldPosition;
+
+                    if (Mathf.Abs(direction.x) > Mathf.Abs(direction.y))
+                    {
+                        if (direction.x > 0)
+                            rotation.z = 0;
+                        else
+                            rotation.z = 180;
+                    }
+                    else
+                    {
+                        if (direction.y > 0)
+                            rotation.z = 90;
+                        else
+                            rotation.z = 270;
+                    }
+
+                    lastWorldPosition.x = Mathf.RoundToInt(lastWorldPosition.x);
+                    lastWorldPosition.y = Mathf.RoundToInt(lastWorldPosition.y);
+
+
+                    forceFields.Add(Instantiate(forceFieldPrefab, lastWorldPosition, Quaternion.Euler(rotation)) as GameObject);
+                }
+            }
+
+            ++i;
+        }
+    }
+	
+	void Update()
+    {
+        CreateForceFieldUsingSweepMotion();
+        //CreateForceFieldUsingAngle();
 	}
 }
--- Ismar_BaseProject/Assets/Aytek Scripts/Key.cs
+++ Ismar_BaseProject/Assets/Aytek Scripts/Key.cs
@@ -5,31 +5,36 @@ public class Key : MonoBehaviour
 {
     public float speed = 1;
     public Vector3 initialPosition;
+    public Vector3 velocity;
 
 	void Start()
     {
-        rigidbody.velocity = Vector3.right * speed;
+        velocity = Vector3.left * speed;
 	}
 	
 	void Update()
     {
-	
+        transform.position += velocity * Time.deltaTime;
 	}
 
     void OnTriggerEnter(Collider other)
     {
+        Debug.LogError("!");
+
         if (other.name.Equals("Exit"))
         {
             Debug.Log("You succesfully get the key out of the maze.");
         }
         else if (other.name.Equals("ForceField"))
         {
-            rigidbody.velocity = other.transform.right;
+            Debug.Log("!");
+            velocity = other.transform.right;
         }
         else if (other.name.Equals("Wall"))
         {
-            Debug.Log("You failed.");
+            Debug.Log("You failed. Try Again");
             transform.position = initialPosition;
+            velocity = Vector3.left * speed;
         }
     }
 }
