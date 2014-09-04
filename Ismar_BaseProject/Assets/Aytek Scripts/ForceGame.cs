70a57642e0302357e388844245ba2f51496a62f4
Aytek Aman <aytekaman@gmail.com>
2014-09-04 12:00:09 +0200

    Added direction calculations for force fields

--- Ismar_BaseProject/Assets/Aytek Scripts/ForceGame.cs
+++ Ismar_BaseProject/Assets/Aytek Scripts/ForceGame.cs
@@ -25,7 +25,26 @@ public class ForceGame : MonoBehaviour
 
                 if (Physics.Raycast(ray, out hit))
                 {
-                    forceFields.Add(Instantiate(forceFieldPrefab, hit.point, transform.rotation) as GameObject);
+                    Vector3 rotation = Vector3.zero;
+
+                    Vector2 toTarget = hit.point - arCamera.transform.position;
+                    if (Mathf.Abs(toTarget.x) > Mathf.Abs(toTarget.y))
+                    {
+                        if (toTarget.x > 0)
+                            rotation.z = 0;
+                        else
+                            rotation.z = 180;
+                    }
+                    else
+                    {
+                        if (toTarget.y > 0)
+                            rotation.z = 90;
+                        else
+                            rotation.z = 270;
+                    }
+
+
+                    forceFields.Add(Instantiate(forceFieldPrefab, hit.point, Quaternion.Euler(rotation)) as GameObject);
                 }
             }
 
