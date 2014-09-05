f67bbbd83f089d24db0292c60f47c4b8403817d1
Rameos <martin.dechant@web.de>
2014-09-05 19:03:29 +0200

    add tetris people

---
+++ Ismar_ARLab/Assets/TetrisGuy_BlinkEye.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TetrisGuy_BlinkEye : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Ismar_ARLab/Assets/TetrisGuy_SelectColor.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TetrisGuy_SelectColor : MonoBehaviour {
+
+    [SerializeField]
+    private Material[] colors;
+
+
+	// Use this for initialization
+	void Start () {
+        int randomCol = Random.Range(0, colors.Length - 1);
+        renderer.material = colors[randomCol];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
