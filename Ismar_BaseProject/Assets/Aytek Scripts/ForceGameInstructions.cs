d445b34ebe3fca8b51eb46cc7e885e02d76e2577
Rameos <martin.dechant@web.de>
2014-09-09 14:28:25 +0200

    add Fancy INtroscreen

--- Ismar_BaseProject/Assets/Aytek Scripts/ForceGameInstructions.cs
+++ Ismar_BaseProject/Assets/Aytek Scripts/ForceGameInstructions.cs
@@ -1,15 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[ExecuteInEditMode]
 public class ForceGameInstructions : MonoBehaviour
 {
-    string instructionText = @"You should move the blue ball to the end of the maze by creating force fields. Avoid walls. Use swipe motions to determine the direction of the force fields. Tap screen to start.";
 
-	void Start()
-	{
-	
-	}
-	
+    [SerializeField]
+    Texture tutorialTexture;
+
+    public float widthX;
+    public float heightX;
+    //[SerializeField]
+    //string headLine = @"The Forcegame";
+
+    //[SerializeField]
+    //string instructionText = @"You should move the blue ball to the end of the maze by creating force fields. Avoid walls. Use swipe motions to determine the direction of the force fields. Tap screen to start.";
+
+    //[SerializeField]
+    //GUIStyle styleHeadline;
+
+    //[SerializeField]
+    //GUIStyle standardFont;
+
+
 	void Update()
 	{
         int i = 0;
@@ -29,8 +42,11 @@ public class ForceGameInstructions : MonoBehaviour
 
     void OnGUI()
     {
-        GUI.skin.box.wordWrap = true;
-        GUI.skin.box.fontSize = Screen.height / 20;
-        GUI.Box(new Rect(Screen.width / 8f, Screen.height / 8f, (Screen.width * 6f) / 8f, (Screen.height * 6f) / 8f), instructionText);
+        GUI.DrawTexture(new Rect(Screen.width / 8f, Screen.height / 95f, Screen.width*0.67f, Screen.height), tutorialTexture);
+
+        //GUI.Label(new Rect(Screen.width / 8f, Screen.height / 95f, (Screen.width * 6f) / 8f, (Screen.height * 6f)),headLine,styleHeadline);
+        //GUI.skin.box.wordWrap = true;
+        //GUI.skin.box.fontSize = Screen.height / 20;
+        //GUI.Box(new Rect(Screen.width / 8f, Screen.height / 6f, (Screen.width * 6f) / 8f, (Screen.height * 6f) / 8f), instructionText);
     }
 }
