1a0b4bfdafc7f5b94417a4a291319a31654ba9a3
Aytek Aman <aytekaman@gmail.com>
2014-09-09 17:56:02 +0200

    Updates to the GameSelector

--- Ismar_BaseProject/Assets/Aytek Scripts/GameSelector.cs
+++ Ismar_BaseProject/Assets/Aytek Scripts/GameSelector.cs
@@ -5,6 +5,7 @@ public class GameSelector : MonoBehaviour
 {
     bool isMarkerLocated = false;
     string sceneName = "";
+    string notification = "";
 
 	void Start()
 	{
@@ -25,14 +26,26 @@ public class GameSelector : MonoBehaviour
 
         if (name == "Lens1" || name == "Lens2" || name == "Lens3")
         {
-            Debug.Log("Starting the Magnifying Glass Game");
+            notification = "Starting the Magnifying Glass game";
             sceneName = "MagnifyIntro";
             Invoke("LoadGame", 1);
         }
         else if (name == "maze3" || name == "MazeTarget2")
         {
-            Debug.Log("Starting the Force Game");
+            notification = "Starting the Energymaze game";
             sceneName = "ForceGameInstructions";
+            Invoke("LoadGame", 1);
+        }
+        else if (name == "BackCover")
+        {
+            notification = "Starting the Light game";
+            sceneName = "LibraryInfo";
+            Invoke("LoadGame", 1);
+        }
+        else if (name == "PrincessMarker")
+        {
+            notification = "Starting the GPS game";
+            sceneName = "GPSscene";
             Invoke("LoadGame", 1);
         }
     }
@@ -43,7 +56,7 @@ public class GameSelector : MonoBehaviour
         {
             GUI.skin.box.wordWrap = true;
             GUI.skin.box.fontSize = Screen.height / 20;
-            GUI.Box(new Rect(Screen.width / 8f, Screen.height / 8f, (Screen.width * 6f) / 8f, (Screen.height * 6f) / 8f), "Starting the " + sceneName);
+            GUI.Box(new Rect(Screen.width / 8f, Screen.height / 8f, (Screen.width * 6f) / 8f, (Screen.height * 6f) / 8f), notification);
         }
     }
 
