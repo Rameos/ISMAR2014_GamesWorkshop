867e03bcea98f7edbffe7eff3495d89bcd16e917
Frieder Pankratz <pankratz@in.tum.de>
2014-09-09 15:21:28 +0200

    global data object with enums for mini games
    Use:
    GlobalData.instance.isGameSolved(MiniGame.GPSGame)

---
+++ Ismar_BaseProject/Assets/Scripts/GlobalData.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Enum;
+
+public enum MiniGame
+{
+	MagnifyingLense0 = 0,
+	MagnifyingLense1,
+	MagnifyingLense2,
+	MagnifyingLenseComplete,
+	EnergyMaze0,
+	EnergyMaze1,
+	Library,
+	GPSGame
+};
+
+public class GlobalData  {
+
+	#region Constructor
+	
+	private static GlobalData instance;
+
+	
+	public static GlobalData Instance
+	{
+		get
+		{
+			if (instance == null)
+			{
+				new GlobalData();
+			}
+			return instance;
+		}
+	}
+	
+	#endregion
+	#region GameResults
+
+	public GlobalData()
+	{
+		if (instance != null)
+		{
+			return;
+		}
+
+	
+
+
+		instance = this;
+	}
+
+	public bool isGameSolved(MiniGame game) {
+		string key = "solvedGame_"+ game.ToString();
+
+		if(PlayerPrefs.HasKey(key))
+		{
+			return PlayerPrefs.GetInt(key) != 0;
+		} 
+		return false;
+
+	}
+
+	public void gameSolved(MiniGame game) {
+		string key = "solvedGame_"+ game.ToString();
+		PlayerPrefs.SetInt(1);
+	}
+
+	public float getGameValue(MiniGame game, string keySuffix){
+		string key =  game.ToString()+keySuffix;
+		
+		if(PlayerPrefs.HasKey(key))
+		{
+			return PlayerPrefs.GetFloat(key);
+		} 
+		return 0f;
+	}
+
+	public void setGameValue(MiniGame game, string keySuffix, float value){
+		string key =  game.ToString()+keySuffix;
+		PlayerPrefs.SetFloat(key, value);
+	}
+
+
+	
+	#endregion
+
+
+}
