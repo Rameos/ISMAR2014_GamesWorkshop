4c778772903968451ec34f3e59e399c7526cf734
Rameos <martin.dechant@web.de>
2014-09-10 18:59:24 +0200

    redesigned the Libary Instruction

--- Ismar_BaseProject/Assets/LibraryIntro.cs
+++ Ismar_BaseProject/Assets/LibraryIntro.cs
@@ -3,6 +3,18 @@ using System.Collections;
 
 public class LibraryIntro : MonoBehaviour {
 
+
+    [SerializeField]
+    Texture tutorialTexture;
+
+    public float widthX;
+    public float heightX;
+
+
+    void OnGUI()
+    {
+        GUI.DrawTexture(new Rect(Screen.width / 8f, Screen.height / 95f, Screen.width * 0.67f, Screen.height), tutorialTexture);
+    }
 	// Use this for initialization
 	void Start () {
 	
