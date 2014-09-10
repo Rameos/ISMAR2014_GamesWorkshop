42ec0be6b8a3860a2e6dbae2a227bf179c869614
Rameos <martin.dechant@web.de>
2014-09-10 20:26:31 +0200

    add MainMenuVisualisation

--- Ismar_BaseProject/Assets/Aytek Scripts/GameSelector.cs
+++ Ismar_BaseProject/Assets/Aytek Scripts/GameSelector.cs
@@ -1,22 +1,46 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[ExecuteInEditMode]
 public class GameSelector : MonoBehaviour
 {
     bool isMarkerLocated = false;
     string sceneName = "";
     string notification = "";
 
-    bool showStory = true;
+    bool showStory = false;
     bool showInstructions = false;
     bool showMap = false;
-    bool showProgress = false;
+    bool showProgress = true;
 
-    string instructionsText = "Try to locate to posters to launch the games. You can use the MAP for help. Once you located the poster, scan it with your camera to launch the game. You can check the status of the game using STATUS button.";
-    string storyText = "Princess Lineblock is captured ... ";
+    [SerializeField]
+    GUIStyle buttonsMainManu;
+
+    [SerializeField]
+    GUIStyle miniGameName;
+
+    [SerializeField]
+    GUIStyle statusButton;
+
+    [SerializeField]
+    Texture storyBackground;
+    [SerializeField]
+    Texture mapBackground;
+    [SerializeField]
+    Texture instructionBackground;
+
+    [SerializeField]
+    Texture doneQuestButton;
+    [SerializeField]
+    Texture openQuestButton;
+
 
     public Texture passwordTexture;
     public Texture mapTexture;
+
+    private Vector2 anchorLayout;
+    private Vector2 anchorText; 
+   
 
 	void Start()
 	{
@@ -83,7 +107,7 @@ public class GameSelector : MonoBehaviour
             return;
         }
 
-        if (GUI.Button(new Rect(0, 0, Screen.width / 4, Screen.height / 8), "STORY"))
+        if (GUI.Button(new Rect(0, 0, Screen.width / 4, Screen.height / 8), "STORY",buttonsMainManu))
         {
             showStory = true;
             showInstructions = false;
@@ -91,7 +115,7 @@ public class GameSelector : MonoBehaviour
             showProgress = false;
         }
 
-        if (GUI.Button(new Rect(Screen.width / 4, 0, Screen.width / 4, Screen.height / 8), "INSTRUCTIONS"))
+        if (GUI.Button(new Rect(Screen.width / 4, 0, Screen.width / 4, Screen.height / 8), "INSTRUCTIONS", buttonsMainManu))
         {
             showStory = false;
             showInstructions = true;
@@ -99,7 +123,7 @@ public class GameSelector : MonoBehaviour
             showProgress = false;
         }
 
-        if (GUI.Button(new Rect(Screen.width / 2, 0, Screen.width / 4, Screen.height / 8), "MAP"))
+        if (GUI.Button(new Rect(Screen.width / 2, 0, Screen.width / 4, Screen.height / 8), "MAP", buttonsMainManu))
         {
             showStory = false;
             showInstructions = false;
@@ -107,7 +131,7 @@ public class GameSelector : MonoBehaviour
             showProgress = false;
         }
 
-        if (GUI.Button(new Rect((Screen.width  * 3f) / 4f, 0, Screen.width / 4, Screen.height / 8), "STATUS"))
+        if (GUI.Button(new Rect((Screen.width * 3f) / 4f, 0, Screen.width / 4, Screen.height / 8), "STATUS", buttonsMainManu))
         {
             showStory = false;
             showInstructions = false;    
@@ -128,35 +152,55 @@ public class GameSelector : MonoBehaviour
 
         if (showStory)
         {
-            GUI.Box(new Rect(Screen.width / 4f, Screen.height / 4f, Screen.width / 2f, Screen.height / 2f), storyText);
+            GUI.DrawTexture(new Rect(Screen.width * 0.05f, Screen.height / 4f, Screen.width * 0.85f, Screen.width / 2.85f), storyBackground);
         }
 
         if (showProgress)
         {
-            string progressText = "";
+            // Init Points
 
-            string gpsGameStatus = GlobalData.Instance.isGameSolved(MiniGame.GPSGame) ? "<color=green>COMPLETE</color>" : "<color=red>INCOMPLETE</color>";
-            string energyMazeGameStatus = GlobalData.Instance.isGameSolved(MiniGame.EnergyMaze) ? "<color=green>COMPLETE</color>" : "<color=red>INCOMPLETE</color>";
-            string magnifyingLenseGameStatus = GlobalData.Instance.isGameSolved(MiniGame.MagnifyingLense) ? "<color=green>COMPLETE</color>" : "<color=red>INCOMPLETE</color>";
-            string libraryGameStatus = GlobalData.Instance.isGameSolved(MiniGame.Library) ? "<color=green>COMPLETE</color>" : "<color=red>INCOMPLETE</color>";
+            anchorLayout = new Vector2(Screen.width * 0.7f, Screen.height * 0.3f);
+            anchorText = new Vector2(Screen.width * 0.25f, Screen.height * 0.3f);
+            miniGameName.fontSize = (int)(Screen.height * 0.05f);
+            
+            //GPS Game
+            GUI.Label(new Rect(anchorText.x, anchorText.y, Screen.width * 0.48f, Screen.width * 0.05f), "The melodic safe", miniGameName);
+            GUI.DrawTexture(new Rect(anchorLayout.x, anchorLayout.y, Screen.width * 0.05f, Screen.width * 0.05f), drawStatusOfGame(MiniGame.GPSGame));
+            
+            //EnergyGame
+            GUI.Label(new Rect(anchorText.x, anchorText.y+Screen.height*0.15f, Screen.width * 0.48f, Screen.width * 0.05f), "EnergyGame", miniGameName);
+            GUI.DrawTexture(new Rect(anchorLayout.x, anchorLayout.y + Screen.height * 0.15f, Screen.width * 0.05f, Screen.width * 0.05f), drawStatusOfGame(MiniGame.EnergyMaze));
 
-            progressText += "GPS GAME: " + gpsGameStatus + "\n";
-            progressText += "ENERGY MAZE: " + energyMazeGameStatus + "\n";
-            progressText += "MAGNIFYING GLASS: " + magnifyingLenseGameStatus + "\n";
-            progressText += "LIBRARY GAME: " + libraryGameStatus + "\n";
+            //Magnifying Glass
+            GUI.Label(new Rect(anchorText.x, anchorText.y + Screen.height * 0.3f, Screen.width * 0.48f, Screen.width * 0.05f), "The lost source", miniGameName);
+            GUI.DrawTexture(new Rect(anchorLayout.x, anchorLayout.y + Screen.height * 0.3f, Screen.width * 0.05f, Screen.width * 0.05f), drawStatusOfGame(MiniGame.MagnifyingLense));
 
-            GUI.Box(new Rect(Screen.width / 4f, Screen.height / 4f, Screen.width / 2f, Screen.height / 2f), progressText);
+            //EnergyGame
+            GUI.Label(new Rect(anchorText.x, anchorText.y + Screen.height * 0.45f, Screen.width * 0.48f, Screen.width * 0.05f), "The missing candle", miniGameName);
+            GUI.DrawTexture(new Rect(anchorLayout.x, anchorLayout.y + Screen.height * 0.45f, Screen.width * 0.05f, Screen.width * 0.05f), drawStatusOfGame(MiniGame.Library));
+           
         }
 
         if (showInstructions)
         {
-            GUI.Box(new Rect(Screen.width / 4f, Screen.height / 4f, Screen.width / 2f, Screen.height / 2f), instructionsText);
+            GUI.DrawTexture(new Rect(Screen.width * 0.05f, Screen.height / 4f, Screen.width * 0.85f, Screen.width / 2.85f), instructionBackground);
         }
 
         if (showMap)
         {
-            GUI.DrawTexture(new Rect(0, Screen.height / 4f, Screen.width, Screen.width / 4), mapTexture);
+            GUI.DrawTexture(new Rect(Screen.width * 0.05f, Screen.height / 4f, Screen.width * 0.85f, Screen.width / 2.85f), mapBackground);
         }
+    }
+
+    private Texture drawStatusOfGame(MiniGame type)
+    {
+        if(GlobalData.Instance.isGameSolved(MiniGame.GPSGame))
+        {
+            return doneQuestButton;
+
+        }
+
+        return openQuestButton;
     }
 
     void LoadGame()
